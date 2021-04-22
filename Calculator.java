61ce1f62026eb848ff8a73713c7c753375759e7d
Dinesh <dinesh.skini@gmail.com>
2021-04-23 00:17:07 +0530

    My first commit

---
+++ Calculator.java
@@ -0,0 +1,30 @@
+public class Calculator {
+
+    static int add(int a, int b) {
+        return 0;
+    }
+
+    static int sub(int a, int b) {
+        return 0;
+    }
+
+    static int mul(int a, int b) {
+        return 0;
+    }
+
+    static int div(int a, int b) {
+        return 0;
+    }
+
+    static int mod(int a, int b) {
+        return 0;
+    }
+
+    public static void main(String args[]) {
+        System.out.println(add(5, 4));
+        System.out.println(sub(5, 4));
+        System.out.println(mul(5, 4));
+        System.out.println(div(5, 4));
+        System.out.println(mod(5, 4));
+    }
+}
