5dce864de4d8577ca2ecfeedbf35d3a7b336f4d6
Dinesh <dinesh.skini@gmail.com>
2021-04-23 00:23:12 +0530

    Second commit

--- Calculator.java
+++ Calculator.java
@@ -1,27 +1,42 @@
 public class Calculator {
 
+    /*
+     * Rahul
+     */
     static int add(int a, int b) {
         return 0;
     }
 
+    /*
+     * Raushan
+     */
     static int sub(int a, int b) {
         return 0;
     }
 
+    /*
+     * Prashant
+     */
     static int mul(int a, int b) {
         return 0;
     }
 
+    /*
+     * Pushpanjali
+     */
     static int div(int a, int b) {
         return 0;
     }
 
+    /*
+     * Rishab
+     */
     static int mod(int a, int b) {
         return 0;
     }
 
     public static void main(String args[]) {
-        System.out.println(add(5, 4));
+        System.out.println(add(9, 4));
         System.out.println(sub(5, 4));
         System.out.println(mul(5, 4));
         System.out.println(div(5, 4));
